bc25aba1cf64f82716e988cc6c94a17bb2ba712f
John Fields <jjfields24@outlook.com>
2022-01-02 05:21:38 -0500

    removed unwanted code

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const usersRoutes = require('./routes/users.js');
-const authRoutes = require('./routes/auth.js');
 
 module.exports = () => {
   const app = express();
   
   //middleware
   app.use(express.json());
-  app.use(usersRoutes);
-  app.use(authRoutes);
+  app.use();
 
   return app;
 };
