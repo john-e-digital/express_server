44283ecc48b7b226456dbc4b834b36985fff7ff1
John Fields <jjfields24@outlook.com>
2022-01-02 05:13:43 -0500

    added app code

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const usersRoutes = require('./routes/users.js');
+const authRoutes = require('./routes/auth.js');
+
+module.exports = () => {
+  const app = express();
+  
+  //middleware
+  app.use(express.json());
+  app.use(usersRoutes);
+  app.use(authRoutes);
+
+  return app;
+};
